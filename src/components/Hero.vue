14f66e51b0da602de9270cc1a57d14440139991d
Oliver Cox <olivercoxdesign@gmail.com>
2020-04-25 19:25:02 +0100

    first commit

---
+++ src/components/Hero.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="h-screen bg-black text-white">
+        HERO COMPONENT
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Hero',
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
