357780871525439b6f8aa0b7f2216b08521564e0
Oliver Cox <olivercoxdesign@gmail.com>
2020-04-26 15:20:35 +0100

    missing copy

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -2,12 +2,12 @@
     <div class="h-auto md:h-full bg-black py-10 md:py-20">
         <div class="container mx-auto max-w-4xl text-white">
 
-            <div class="w-32 h-32 mx-auto mb-4"> 
+            <div class="w-32 h-32 mx-auto mb-4" > 
                 <img class="img--responsive" alt="midnight caller book cover" src="../assets/images/logo-white.png">
             </div>
 
             <div class="font-display px-4 md:px-0">
-                <h1 class="text-2xl md:text-6xl font-bold mb-6">Tales from Floor Fifty-Four</h1>
+                <h1 class="text-2xl md:text-6xl font-bold mb-6" id='signup_anchor'>Tales from Floor Fifty-Four</h1>
                 <p class="text-base md:text-2xl font-bold mb-4">
                     All nations have a deep, dark pit to hide their secrets.
                 </p>
@@ -20,7 +20,7 @@
                     Creatures that would break the world.<br>
                     And Kevin. He's there too.
                 </p>
-                <p class="text-base md:text-2xl mb-8">
+                <p class="text-base md:text-2xl mb-8" >
                     Sign up for the Floor Fifty-Four Newsletter and receive free e-books, discounts and the chance to be a Beta-Reader!
                 </p>
             </div>
--- src/components/Tile.vue
+++ src/components/Tile.vue
@@ -4,7 +4,8 @@
             <div class="">
                 <div class="py-8 px-2 max-w-md">
                     <h2 class="font-bold text-2xl sm:text-4xl font-display mb-3">Book 1 - Midnight Caller</h2>
-                    <p class="font-body text-grey italic text-base sm:text-xl">People are disappearing on the Yorkshire Moors. Agents Radford and Stone are tasked with investigating the lead suspect; a 320 year old vampire who has breached the terms of his release into our world. But when it becomes clear he is not the culprit, will Radford and Stone be able to find the true killer? Or will the vampire take the fall?</p>
+                    <p class="font-body text-grey italic text-base sm:text-xl mb-6">People are disappearing on the Yorkshire Moors. Agents Radford and Stone are tasked with investigating the lead suspect; a 320 year old vampire who has breached the terms of his release into our world. But when it becomes clear he is not the culprit, will Radford and Stone be able to find the true killer? Or will the vampire take the fall?</p>
+                    <p class="font-body text-grey text-base sm:text-xl">Due for release in Autumn 2020<br> <a href="#signup_anchor" class="font-bold">Sign up</a> to become a Beta Reader!</p>
                 </div>
             </div>
             <div class="px-4 md:px-0 md:ml-auto">
